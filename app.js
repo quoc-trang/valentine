f0acac376819d6ac2388e22e63304c03d84b096e
quoctrang <quoctrang2701@gmail.com>
2025-02-13 21:31:37 +0700

    change img

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const text = ['are you sure?', 'really sure?', 'think again!', 'last chance', 'surely not?', 'you might regret it', 'give it another thought', 'are you absolutely certain?', 'this could be a mistake!', 'have a heart!', `dont't be so cold`, 'change of heart?', `wouldn't you reconsider?`, 'is that your final answer?', `you're breaking my heart`, `you're breaking my heart:(`]
-const yesUrl = './freefire.gif'
+const yesUrl = 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMHlwbWxxM2lwcWYzeHI5eHZ1eG96czhseWZuNHozMHU1bW93dTJtZCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/KztT2c4u8mYYUiMKdJ/giphy.gif'
 const noBtn = document.querySelector('button.no')
 const yesBtn = document.querySelector('button.yes')
 
@@ -16,7 +16,7 @@ yesBtn.addEventListener('click', function(){
     noBtn.style.display = 'none'
     const dogImg = document.querySelector('img.dog-image')
     const textH1 = document.querySelector('h1.text')
-    textH1.style.display = 'none'
+    textH1.textContent = 'OkAY, YAY'
 
     dogImg.src = yesUrl
 
